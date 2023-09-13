862b8f6d66cef6c3c8a0e5d1927955961caf8ebb
paebukoa is cool <gabriel.flau.pr@gmail.com>
2023-09-13 23:34:24 +0000

    register page

--- src/routes/register/Register.js
+++ src/routes/register/Register.js
@@ -1,7 +1,127 @@
-function Register() {
-    return (
-        <h1>Pagina de Registro</h1>
-    );
+import { useState } from 'react'
+import './Register.css'
+
+import socket from '../../services/socket';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+  const navigate = useNavigate()
+
+  let [ username, setUsername ] = useState('');
+  let [ nickname, setNickname ] = useState('');
+  let [ password, setPassword ] = useState('');
+  let [ confirmPassword, setConfirmPassword ] = useState('');
+  let [ isSubmiting, setIsSubmiting ] = useState(false);
+  let [ error, setError ] = useState('');
+
+  function focusNext() {
+    console.log('focusing next')
+    const inputs = Array.from(document.querySelectorAll('.loginField > input'))
+    const idx = inputs.indexOf(document.activeElement) + 1
+    if (idx === inputs.length) {
+      document.activeElement.blur()
+      document.querySelector('.submitLogin').click()
+    } else {
+      inputs[idx].focus()      
+    }
+  }
+
+  function loginSubmitHandler() {
+    setError('');
+    setIsSubmiting(true);
+
+    if (password !== confirmPassword) {
+      setIsSubmiting(false)
+      return setError('As senhas não coincidem.');
+    }
+
+    socket.emit("register", { username, nickname, password }, d => {
+      if (d.err) {
+        let errMsg;
+
+        switch (d.errType) {
+          case "FIELDS_NOT_FILLED": errMsg = "Todos os campos devem ser preenchidos."; break;
+          case "USERNAME_INVALID": errMsg = "O nome de usuário é inválido."; break;
+          case "USERNAME_IN_USE": errMsg = "O nome de usuário inserido já está em uso."; break;
+          default: errMsg = "Um erro desconhecido ocorreu. Código do erro: " + d.errType 
+        }
+        setIsSubmiting(false)
+        return setError(errMsg)
+      }
+
+      localStorage.setItem("token", d.user.token)
+      navigate("/app")
+    })
+  }
+
+  return (
+  <div className="container login">
+    <div className="loginTabs">
+      <div className="loginTab">
+        <h1 className="tabTitle">Registrar uma conta</h1>
+        <div className="loginFields">
+          <div className="loginField input required">
+            <span className="fieldName">Nome de usuário</span>
+            <input 
+            type="text" 
+            className="fieldInput" 
+            disabled={isSubmiting}
+            onKeyDown={ e => e.key === "Enter" && focusNext() }
+            onChange={ (e) => setUsername(e.target.value) }
+            />
+          </div>
+          <div className="loginField input">
+            <span className="fieldName">Apelido</span>
+            <input 
+            type="text" 
+            className="fieldInput" 
+            disabled={isSubmiting}
+            onKeyDown={ e => e.key === "Enter" && focusNext() }
+            onChange={ (e) => setNickname(e.target.value) }
+            />
+          </div>
+          <div className="loginField input required">
+            <span className="fieldName">Senha</span>
+            <input 
+            type="password" 
+            className="fieldInput" 
+            disabled={isSubmiting}
+            onKeyDown={ e => e.key === "Enter" && focusNext() }
+            onChange={ (e) => setPassword(e.target.value) }
+            />
+          </div>
+          <div className="loginField input required">
+            <span className="fieldName">Confirmar senha</span>
+            <input 
+            type="password" 
+            className="fieldInput" 
+            disabled={isSubmiting}
+            onKeyDown={ e => e.key === "Enter" && focusNext() }
+            onChange={ (e) => setConfirmPassword(e.target.value) }
+            />
+          </div>
+        </div>
+        <div className="tabBottom">
+          <span className="errorMessage">
+            { error !== '' && error }
+          </span>
+          <button 
+          className="registerButton"
+          onClick={() => navigate("/login")}
+          >
+          Entrar em uma conta
+          </button>
+          { !isSubmiting ? (
+            <button className="submitLogin" onClick={loginSubmitHandler}>Registrar</button>
+          ) : (
+            <button className="submitLogin" disabled><div className="spinningDisk"></div></button>
+          )
+          }
+        </div>
+      </div>
+    </div>
+  </div>
+);
 }
 
-export default Register;
+export default Login;
