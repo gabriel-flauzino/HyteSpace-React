01b40cba2a25de96f5ebdc846705a0524fe5d138
paebukoa <gabriel.flau.pr@gmail.com>
2023-10-02 00:25:40 -0300

    Create post

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { BiSolidUser, BiSolidPaperPlane } from 'react-icons/bi';
+import { IoMdSend } from "react-icons/io";
 
 import './Home.css';
 import socket from './../../services/socket';
@@ -12,6 +13,7 @@ function Home() {
   const [user, setUser] = useState(null)
   const [friendships, setFriendships] = useState(null)
   const [feedPosts, setFeedPosts] = useState(null)
+  const [selectedType, setSelectedType] = useState("global");
   const [auth] = useState(localStorage.getItem('token'))
 
   let currentHours;
@@ -29,7 +31,7 @@ function Home() {
         // auth is ok
         setUser(d.user)
         loadData().then(() => setDataLoaded(true)); // loads data
-        
+
         socket.on('updateUser', refreshUser)
         socket.on('updateFriendships', refreshFriendships)
         socket.on('updateFeed', refreshFeed)
@@ -94,7 +96,7 @@ function Home() {
       <div className="content">
         <nav className='navbar'>
           <div className="navStart">
-            
+
           </div>
           <div className="navMiddle">
 
@@ -116,72 +118,109 @@ function Home() {
             }, {user?.nickname}!
           </h1>
           <div className="friendsContainer">
-            {
-              // loading friendships with live updating
-              friendships?.[0] ? (
-                <div className="friendsList">
-                  {friendships.map(x => {
-                    return (
-                      <div className="friendUser" key={i++}>
-                        <div className="friendAvatar">
-                          <BiSolidUser className="friendIcon" size={50} color="#55585e" />
-                        </div>
-                        <div className="friendLabels">
-                          <span className="friendNickname">{x.user?.nickname}</span>
-                          <span className="friendUsername">@{x.user?.username}</span>
-                        </div>
-                      </div>
-                    )
-                  })}
-                </div>
-              ) : (
-                <div className="friendsEmptyContainer">
-                  <div className="friendsEmptyBox">
-                    <BiSolidPaperPlane className='friendsEmptyIcon' size={50} color="#fff" />
-                    <span className="friendsEmptyMessage">Não há amigos :(</span>
-                  </div>
-                </div>
-              )
-            }
-          </div>
-          <div className="contentContainer">
-            <div className="leftSidebar"></div>
-            <div className="feedContainer">
+            <div className="friendsContainerLabel">Seus amigos</div>
+            <div className="friendsBox">
               {
-                feedPosts === null
-                ? (
-                  <div className="feedLoading">
-                    <div className="spinningDisk"></div>
+                // loading friendships with live updating
+                friendships?.[0] ? (
+                  <div className="friendsList">
+                    {friendships.map(x => {
+                      return (
+                        <div className="friendUser" key={i++}>
+                          <div className="friendAvatar">
+                            <BiSolidUser className="friendIcon" size={50} color="#55585e" />
+                          </div>
+                          <div className="friendLabels">
+                            <span className="friendNickname">{x.user?.nickname}</span>
+                            <span className="friendUsername">@{x.user?.username}</span>
+                          </div>
+                        </div>
+                      )
+                    })}
+                  </div>
+                ) : (
+                  <div className="friendsEmptyContainer">
+                    <div className="friendsEmptyBox">
+                      <BiSolidPaperPlane className='friendsEmptyIcon' size={50} color="#fff" />
+                      <span className="friendsEmptyMessage">Não há amigos :(</span>
+                    </div>
                   </div>
                 )
-                : !feedPosts.length
-                  ? (
-                    <div className="emptyFeedContainer">
-                      <div className="emptyFeedBox">
-                        <span className="emptyFeedMessage">Não há postagens.</span>
-                      </div>
-                    </div>
-                  )
-                  : (
-                    <div className="postsContainer">
-                      { feedPosts.map(x => (
-                        <div className="feedPost">
-                          <div className="avatarBox">
-                            <BiSolidUser className="friendIcon" size={50} color="#fff" />
-                          </div>
-                          <div className="postBox">
-                            <div className="authorBox">
-                              
-                            </div>
-                            <div className="contentBox"></div>
-                          </div>
-                        </div>
-                      )) }
-                    </div>
-                  )
               }
             </div>
-            <div className="rightSidebar"></div>
+          </div>
+          <div className="contentContainer">
+            <div className="sidebar left"></div>
+            <div className="feedContainer">
+              <div className="createPostContainer">
+                <div className="createPostBox top">
+                  <div className="cpUserInfoBox">
+                    <div className="cpAvatar"><BiSolidUser className="friendIcon" size="100%" color="#55585e" /></div>
+                    <div className="cpUserNamesBox">
+                      <span className="cpNickname">{user?.nickname}</span>
+                      <span className="cpUsername">@{user?.username}</span>
+                    </div>
+                  </div>
+                  <div className="cpPreferencesBox">
+                    <div className="cpPostType">
+                      <select name="postType" id="postTypeSelect" onChange={e => setSelectedType(e.target.value)}>
+                        <option value="global" selected>Global</option>
+                        <option value="private">Privado</option>
+                      </select>
+                    </div>
+                    <span className='cpPrefDescription'>{selectedType == "global" ? "Todo mundo poderá visualizar" : "Apenas seus amigos poderão visualizar"}</span>
+                  </div>
+                </div>
+                <div className="createPostBox inputsArea">
+                  <textarea name="postContent" id="postContentInput" placeholder="Escreva uma postagem..."></textarea>
+                  <button id="submitPost"><IoMdSend size={25} color='#e7e7e7' /></button>
+                </div>
+                <div className="createPostBox markdownToolbox">
+                  <button className='mdTool' id="markdownBoldTool"><strong>B</strong></button>
+                  <button className='mdTool' id="markdownUnderlineTool"><u>N</u></button>
+                  <button className='mdTool' id="markdownItalicTool"><i>I</i></button>
+                  <button className='mdTool' id="markdownStrokeTool"><s>S</s></button>
+                  <button className='mdTool' id="markdownH1Tool"><strong>h1</strong></button>
+                  <button className='mdTool' id="markdownH2Tool"><strong>h2</strong></button>
+                </div>
+              </div>
+              <div className="feedBox">
+                {
+                  feedPosts === null
+                    ? (
+                      <div className="feedLoading">
+                        <div className="spinningDisk"></div>
+                      </div>
+                    )
+                    : !feedPosts.length
+                      ? (
+                        <div className="emptyFeedContainer">
+                          <div className="emptyFeedBox">
+                            <span className="emptyFeedMessage">Não há postagens.</span>
+                          </div>
+                        </div>
+                      )
+                      : (
+                        <div className="postsContainer">
+                          {feedPosts.map(x => (
+                            <div className="feedPost">
+                              <div className="avatarBox">
+                                <BiSolidUser className="friendIcon" size={50} color="#fff" />
+                              </div>
+                              <div className="postBox">
+                                <div className="authorBox">
+
+                                </div>
+                                <div className="contentBox"></div>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      )
+                }
+              </div>
+            </div>
+            <div className="sidebar right"></div>
           </div>
         </main>
       </div>
